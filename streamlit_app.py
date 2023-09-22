5cbbea9a609df51ec99cc7018f79183945e622ab
Fernando Meza <43585972+mezaf@users.noreply.github.com>
2023-09-22 11:21:20 -0300

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.random.randit(0,100,shape=(4,100))
+df = pd.DataFrame(np.random.randit(0,100,shape=(4,100)))
 
 st.line_chart(df)
